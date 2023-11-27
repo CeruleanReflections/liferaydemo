80109568ecfdc79e3b73b23879608d87bd241ede
Matteo <51327181+CeruleanReflections@users.noreply.github.com>
2023-11-27 12:11:16 +0100

    First commit

--- modules/liferay-courses-api/src/main/java/com/liferay/courses/api/LiferayCourse.java
+++
@@ -1,9 +0,0 @@
-package com.liferay.courses.api;
-
-public class LiferayCourse {
-    private String name;
-    private String description;
-
-    public LiferayCourse(String name, String description) {
-    }
-}
