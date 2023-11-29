5f148a8751543a26bd6f2c064d26c42be9e8ae17
Matteo <51327181+CeruleanReflections@users.noreply.github.com>
2023-11-29 11:19:34 +0100

    Aggiunti API e Service

---
+++ modules/liferay-courses-api/src/main/java/com/liferaybook/courses/api/LiferayCoursesAPI.java
@@ -0,0 +1,12 @@
+package com.liferaybook.courses.api;
+
+import java.util.List;
+
+/**
+ * @author matteo.donnini
+ */
+public interface LiferayCoursesAPI {
+
+    List<LiferayCourse> getCourses();
+
+}
---
+++ modules/liferay-courses-service/src/main/java/com/liferay/courses/service/LiferayCoursesService.java
@@ -0,0 +1,30 @@
+package com.liferay.courses.service;
+
+import com.liferaybook.courses.api.LiferayCourse;
+import com.liferaybook.courses.api.LiferayCoursesAPI;
+
+import org.osgi.service.component.annotations.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author matteo.donnini
+ */
+@Component(
+	property = {
+		// TODO enter required service properties
+	},
+	service = LiferayCoursesAPI.class
+)
+public class LiferayCoursesService implements LiferayCoursesAPI {
+	@Override
+	public List<LiferayCourse> getCourses() {
+		List<LiferayCourse> courses = new ArrayList<>();
+		courses.add(new LiferayCourse("Corso 1", "Descrizione corso 1"));
+		courses.add(new LiferayCourse("Corso 2","Descrizione corso 2"));
+		courses.add(new LiferayCourse("Corso 3", "Descrizione corso 3"));
+		return courses;
+	}
+
+}
