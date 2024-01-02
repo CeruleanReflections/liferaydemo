ab234867bfa05d79868e2d8ee36e9a6340d513c2
Matteo <51327181+CeruleanReflections@users.noreply.github.com>
2024-01-02 09:32:44 +0100

    Aggiunti meccanismi di error handling e supporto per la portlet localization

--- modules/liferay-courses-web/src/main/java/com/liferay/courses/web/portlet/action/EditCourseMVCActionCommand.java
+++ modules/liferay-courses-web/src/main/java/com/liferay/courses/web/portlet/action/EditCourseMVCActionCommand.java
@@ -3,8 +3,11 @@ package com.liferay.courses.web.portlet.action;
 import com.liferay.courses.web.constants.LiferayCoursesPortletKeys;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferaybook.courses.api.LiferayCoursesAPI;
+import org.jsoup.helper.Validate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -26,14 +29,30 @@ public class EditCourseMVCActionCommand extends BaseMVCActionCommand {
     La modifica viene accettata ugualmente ma i parametri modificati sono vuoti perchè il metodo non trova il campo che cerca.
     * */
     @Override
-    protected void doProcessAction(ActionRequest actionRequest, ActionResponse actionResponse){
-        Long courseId = ParamUtil.getLong(actionRequest, "courseId");
-        String name = ParamUtil.getString(actionRequest, "name");
-        String description = ParamUtil.getString(actionRequest, "description");
-        if (courseId > 0)
-            liferayCoursesAPI.updateCourse(courseId, name, description);
-        else
-            liferayCoursesAPI.saveCourse(name, description);
+    protected void doProcessAction(ActionRequest actionRequest, ActionResponse actionResponse) {
+
+        try {
+            Long courseId = ParamUtil.getLong(actionRequest, "courseId");
+            String name = ParamUtil.getString(actionRequest, "name");
+
+            if (Validator.isBlank(name) || name.trim().length() < 5) {
+                SessionErrors.add(actionRequest, "courseNameNotValid");
+                actionResponse.getRenderParameters().setValue("mvcRenderCommandName", "/courses/edit_course");
+                hideDefaultErrorMessage(actionRequest);
+                return;
+            }
+
+            String description = ParamUtil.getString(actionRequest, "description");
+            if (courseId > 0)
+                liferayCoursesAPI.updateCourse(courseId, name, description);
+            else
+                liferayCoursesAPI.saveCourse(name, description);
+
+        } catch (Exception e) {
+            SessionErrors.add(actionRequest, e.getClass());
+            actionResponse.getRenderParameters().setValue("mvcRenderCommandName", "/courses/edit_course");
+            hideDefaultErrorMessage(actionRequest);
+        }
     }
 
     @Reference
