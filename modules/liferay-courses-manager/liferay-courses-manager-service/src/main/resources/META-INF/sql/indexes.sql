133347cc601f9c8252f7a934544d74d3b92fa54e
Matteo <51327181+CeruleanReflections@users.noreply.github.com>
2024-01-11 12:52:28 +0100

    Clean up

--- modules/liferay-courses-manager/liferay-courses-manager-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/liferay-courses-manager/liferay-courses-manager-service/src/main/resources/META-INF/sql/indexes.sql
@@ -3,5 +3,3 @@ create index IX_93FAE600 on lb_Course (groupId);
 create unique index IX_512C4E29 on lb_Course (name[$COLUMN_LENGTH:100$]);
 
 create index IX_1A136C65 on lb_Lecture (courseId);
-
-create unique index IX_5B451FFE on lb_Teacher (name[$COLUMN_LENGTH:75$]);
